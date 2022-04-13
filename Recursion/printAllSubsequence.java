bca379d0326e7375746345469c42d32089af378a
chirag.ccdps28@gmail.com <chirag.ccdps28@gmail.com>
2022-04-13 10:50:14 +0530

    printing all subsequences

---
+++ Recursion/printAllSubsequence.java
@@ -0,0 +1,28 @@
+package Recursion;
+
+import java.util.ArrayList;
+
+class printAllSubsequence {
+    public static void main(String[] args) {
+        int[] arr = {3,1,2,4};
+        int n = arr.length;
+        ArrayList<Integer> ds = new ArrayList<>(); 
+        print(0, arr, ds, n);
+    }
+    public static void print(int idx, int[] arr, ArrayList<Integer> ds, int n) {
+        if (idx == n) {
+            if(ds.size() != 0) {
+                for (int x : ds) {
+                    System.out.print(x + " ");
+                }
+                System.out.println();
+            }
+            return;
+        }
+
+        ds.add(arr[idx]);
+        print(idx+1, arr, ds, n);
+        ds.remove(ds.indexOf(arr[idx]));
+        print(idx+1, arr, ds, n);
+    }
+}
